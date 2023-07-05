79164de3abd9c876fbd94567aee4fb26895f867b
Vadym Frolov <vdfroloff@gmail.com>
2023-07-05 19:56:21 +0300

    WORK:
    - HomePage created
    - TweetsPage created
    - global.css styles added
    - theme for styled system added

---
+++ src/utils/theme.ts
@@ -0,0 +1,53 @@
+export const theme = {
+  colors: {
+    white: '#fff',
+    black: '#000',
+    deepGray: '#373737',
+    greyViolet: '#ebd8ff',
+    greyVioletHover: '#d8c4ee',
+    greenAccent: '#5cd3a8',
+    greenAccentHover: '#51c59b',
+    violetLight: '#5f35c1',
+    violetDark: '#4b2a99',
+    userCardGradient: 'linear-gradient(114.99deg, #471ca9 -0.99%, #5736a3 54.28%, #4b2a99 78.99%)',
+    addGradient: 'linear-gradient(114.99deg, #4b2a99 -0.99%, #5a28cd 54.28%, #4b2a99 78.99%)',
+    textGradient: 'linear-gradient(to left, #5f35c1 20%, #5cd3a8 40%, #5cd3a8 60%, #5f35c1 80%)',
+  },
+  fontSizes: {
+    xs: '12px',
+    s: '18px',
+    m: '20px',
+    l: '25px',
+    xl: '90px',
+  },
+  fontWeights: {
+    medium: 500,
+    semiBold: 600,
+  },
+  lineHeights: {
+    body: 1.2,
+  },
+  borders: {
+    NavBtn: '3px solid #ebd8ff',
+    NavBtnHover: '3px solid #5cd3a8',
+    LoadMoreBtn: '3px solid #5f35c1',
+    LoadMoreBtnHover: '3px solid #5cd3a8',
+    skeleton: '2px dashed #ebd8ff',
+  },
+  radii: {
+    small: '10px',
+    regular: '20px',
+    round: '50px',
+  },
+  shadows: {
+    userCard: '-2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23)',
+    btn: '0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25)',
+    btnHover: '0px 3.43693px 3.43693px rgba(0, 0, 0, 0.35)',
+    addLight: '0px 2.43693px 2.43693px rgba(0, 0, 0, 0.25)',
+    decorElemEllipse:
+      '0px 4.39163px 4.39163px rgba(0, 0, 0, 0.06), inset 0px -2.19582px 4.39163px #ae7be3, inset 0px 4.39163px 3.29372px #fbf8ff',
+    decorElemRectangle:
+      '0px 3.43693px 3.43693px rgba(0, 0, 0, 0.06), inset 0px -1.71846px 3.43693px #ae7be3, inset 0px 3.43693px 2.5777px #fbf8ff',
+  },
+  zIndices: [-1, 0, 1, 2, 3],
+};
