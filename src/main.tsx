2a46936612a5694a258b0cbbec4e0511bfabea26
Vadym Frolov <vdfroloff@gmail.com>
2023-07-05 19:59:20 +0300

    WORK:
    - ThemeProvider wrapped App
    - BrowserRouter wrapped App

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
 import App from './App.tsx';
+import { theme } from './utils/theme.ts';
+
 import './style/global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
