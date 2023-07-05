1194aa730aa14154c335dadc3f3a48981f9cb295
Vadym Frolov <vdfroloff@gmail.com>
2023-07-05 20:13:58 +0300

    WORK:
    - HomePage draft
    - TweetsPage draft

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Box } from '../components/Box/Box';
+
+const HomePage = () => {
+  return (
+    <Box>
+      <h1>Home Page</h1>
+    </Box>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/TweetsPage.tsx
@@ -0,0 +1,11 @@
+import { Box } from '../components/Box/Box';
+
+const TweetsPage = () => {
+  return (
+    <Box>
+      <h1>Tweets Page</h1>
+    </Box>
+  );
+};
+
+export default TweetsPage;
