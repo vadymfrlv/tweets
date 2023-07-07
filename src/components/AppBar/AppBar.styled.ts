807c2848d2c06b0f0e6b6647303d8c64bcecdb6c
Vadym Frolov <vdfroloff@gmail.com>
2023-07-07 19:52:03 +0300

    WORK:
    - AppBar done
    - AppBar style done

--- src/components/AppBar/AppBar.styled.ts
+++ src/components/AppBar/AppBar.styled.ts
@@ -20,3 +20,85 @@ export const Header = styled.header`
   background-image: ${p => p.theme.colors.addGradient};
   box-shadow: ${p => p.theme.shadows.addLight};
 `;
+
+export const Title = styled.h1`
+  position: relative;
+  font-size: ${p => p.theme.fontSizes.l};
+`;
+
+export const SubTitle = styled.h2`
+  position: absolute;
+  top: 37px;
+
+  font-size: ${p => p.theme.fontSizes.s};
+  letter-spacing: 2px;
+
+  background: ${p => p.theme.colors.textGradient};
+  background-size: 200% auto;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+
+  animation: shine 2000ms linear infinite;
+
+  @-webkit-keyframes shine {
+    to {
+      background-position: 200% center;
+    }
+  }
+  @keyframes shine {
+    to {
+      background-position: 200% center;
+    }
+  }
+`;
+
+export const GoToTweetsBtn = styled.button`
+  margin-top: 12px;
+  height: 40px;
+  width: 176px;
+
+  font-weight: ${p => p.theme.fontWeights.semiBold};
+  font-size: ${p => p.theme.fontSizes.m};
+  line-height: ${p => p.theme.lineHeights.body};
+  text-transform: uppercase;
+
+  color: ${p => p.theme.colors.greyViolet};
+  background-color: transparent;
+  border: ${p => p.theme.borders.NavBtn};
+  border-radius: ${p => p.theme.radii.small};
+
+  &:hover,
+  &:focus {
+    color: ${p => p.theme.colors.greenAccent};
+    border: ${p => p.theme.borders.NavBtnHover};
+  }
+`;
+
+export const GoToHomeBtn = styled.button`
+  position: fixed;
+  z-index: ${p => p.theme.zIndices[4]};
+  top: 73px;
+
+  height: 25px;
+  width: 100px;
+
+  font-weight: ${p => p.theme.fontWeights.semiBold};
+  font-size: ${p => p.theme.fontSizes.xs};
+  line-height: ${p => p.theme.lineHeights.body};
+  text-transform: uppercase;
+
+  color: ${p => p.theme.colors.greyViolet};
+  background-color: ${p => p.theme.colors.violetDark};
+  box-shadow: ${p => p.theme.shadows.addLight};
+
+  &:hover,
+  &:focus {
+    color: ${p => p.theme.colors.greenAccent};
+    border: ${p => p.theme.borders.NavBtnHover};
+  }
+`;
+
+export const Arrow = styled.span`
+  display: inline-block;
+  transform: scaleX(-1);
+`;
--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,13 +1,47 @@
 import { startTransition } from 'react';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+
 import { Box } from '../Box/Box';
 
-import { Header } from './AppBar.styled';
+import { Header, Title, SubTitle, GoToTweetsBtn, GoToHomeBtn, Arrow } from './AppBar.styled';
 
 export const AppBar = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleGoToTweets = (): void => {
+    startTransition(() => {
+      navigate('tweets');
+    });
+  };
+  const handleGoToHome = (): void => {
+    startTransition(() => {
+      navigate('/');
+    });
+  };
+
   return (
     <Box>
-      <Header>AppBar</Header>
+      <Header>
+        {location.pathname === '/' && (
+          <GoToTweetsBtn type="button" onClick={handleGoToTweets}>
+            Go to tweets
+          </GoToTweetsBtn>
+        )}
+
+        {location.pathname === '/tweets' && (
+          <Box display="flex" justifyContent="center">
+            <Title>Thrilled you're here</Title>
+            <SubTitle>Tweets are welcoming</SubTitle>
+          </Box>
+        )}
+      </Header>
+
+      {location.pathname === '/tweets' && (
+        <GoToHomeBtn type="button" onClick={handleGoToHome}>
+          <Arrow>&#x27A4;</Arrow> Go home
+        </GoToHomeBtn>
+      )}
       <Outlet />
     </Box>
   );
